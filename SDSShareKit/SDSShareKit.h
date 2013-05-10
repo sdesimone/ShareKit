abe190224d9d334c201ef882af332169cf389280
Sergio De Simone <sdesimone@gmx.net>
2013-05-10 21:19:51 +0200

    added SDSShareKit taget with minimal libs included

---
+++ SDSShareKit/SDSShareKit.h
@@ -0,0 +1,13 @@
+//
+//  SDSShareKit.h
+//  SDSShareKit
+//
+//  Created by sergio on 5/10/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SDSShareKit : NSObject
+
+@end
---
+++ SDSShareKit/SDSShareKit.m
@@ -0,0 +1,13 @@
+//
+//  SDSShareKit.m
+//  SDSShareKit
+//
+//  Created by sergio on 5/10/13.
+//
+//
+
+#import "SDSShareKit.h"
+
+@implementation SDSShareKit
+
+@end
